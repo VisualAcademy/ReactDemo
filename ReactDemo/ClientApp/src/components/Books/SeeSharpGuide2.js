3fd628007ce630ad37ad704d2c217244f8fcc723
RedPlus <redplus@redplus.net>
2020-06-15 03:53:50 +0900

    SeeSharpGuideTitle

---
+++ ReactDemo/ClientApp/src/components/Books/SeeSharpGuide2.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SeeSharpGuideTitle from "./SeeSharpGuideTitle1";
+
+function SeeSharpGuide() {
+    return (
+        <div>
+            <SeeSharpGuideTitle></SeeSharpGuideTitle>
+            <ul>
+                <li>리액트 소개</li>
+                <li>리액트 개발환경구축</li>
+                <li>리액트 HelloWorld</li>
+            </ul>
+        </div>
+    );
+}
+
+export default SeeSharpGuide;
